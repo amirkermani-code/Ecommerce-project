e429bb8d07438d159c31ee23ea742f2fedcf60a6
amirkermani code <amirkermani2021@gmail.com>
2025-12-07 00:26:58 +0330

    This time I created the first component in the components folder and imported it into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Home from "./components/Home"
 
 
 function App() {
 
   return (
     <>
-      <h1 className="font-bold underline text-3xl">Hello World!</h1>
+      <Home />
     </>
   )
 }
